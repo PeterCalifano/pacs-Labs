b6318cf7530d30cae1afc17344198d65aefc0a7c
Carlo de Falco <carlo.defalco@polimi.it>
2025-09-04 16:27:37 +0200

    fix scaling

--- Labs/2025/13-parallel/02-how_to_parallelize/MPM/grid_velocity_field.cpp
+++ Labs/2025/13-parallel/02-how_to_parallelize/MPM/grid_velocity_field.cpp
@@ -8,6 +8,8 @@
 #include <vector>
 
 #include "counter.h"
+#include <omp.h>
+#include <timer.h>
 
 class
 stepper {
@@ -31,8 +33,8 @@ public :
     double dxb, dyb;
   
     //Brownian motion displacements
-    dxb=normal() * std::sqrt (2*D*dt);
-    dyb=normal() * std::sqrt (2*D*dt);
+    dxb=std::sqrt (2*D*dt); // * normal();
+    dyb=std::sqrt (2*D*dt); // * normal();
  
     //update particles positions
     x[n] += vx[n] * dt + dxb;
@@ -49,6 +51,8 @@ public :
 int
 main () {
 
+  cdf::timer::timer_t timer;
+    
   // read data from file
   const std::string filename = "velocity.json";
 
@@ -70,47 +74,74 @@ main () {
   std::normal_distribution<> normal;
   std::function<double ()> noise = [&gen2, &rd2, &normal] () { return normal(gen2); };
 
-  stepper state (p.x, p.y, p.dprops["VX"], p.dprops["VY"], 1., 1.e-5, noise);
+  stepper state (p.x, p.y, p.dprops["VX"], p.dprops["VY"], 1., 0, noise);
   p.init_particle_mesh ();
-  
-  for (int it = 0; it < 1000; ++it) {
 
-    
+#pragma omp parallel default(shared)
+  {
+    for (int it = 0; it < 10; ++it) {
 
-    std::fill(p.dprops["VX"].begin (), p.dprops["VX"].end (), 0.0);
-    std::fill(p.dprops["VY"].begin (), p.dprops["VY"].end (), 0.0);
-    p.g2p (vars, {"vx", "vy"}, {"VX", "VY"});
+#pragma omp single
+      {
+	std::fill(p.dprops["VX"].begin (), p.dprops["VX"].end (), 0.0);
+	std::fill(p.dprops["VY"].begin (), p.dprops["VY"].end (), 0.0);
+	timer.tic("g2p");
+      }
+      
+      p.g2p (vars, {"vx", "vy"}, {"VX", "VY"});
 
-#pragma omp parallel for
-    for (int ip = 0; ip < p.num_particles; ++ip) {
-      state(ip);
-    }
+#pragma omp single
+      {
+	timer.toc("g2p");
+	timer.tic("move partcles");
+      }
+      
+#pragma omp barrier
+#pragma omp for 
+      for (int ip = 0; ip < p.num_particles; ++ip) {
+	//std::cout << "thread " << omp_get_thread_num () << " processing particle " << ip << std::endl;
+	state(ip);
+      }
+      
+#pragma omp barrier
+#pragma omp single
+      {
+	timer.toc("move partcles");
+	timer.tic("init_particle_mesh");
+	p.init_particle_mesh ();
+	timer.toc("init_particle_mesh");
+	std::fill(vars["rho"].begin (), vars["rho"].end (), 0.0);
+	timer.tic("p2g");
+      }
 
-    p.init_particle_mesh ();
-    std::fill(vars["rho"].begin (), vars["rho"].end (), 0.0);
-    p.p2g (vars, {"M"}, {"rho"}, true);
+      p.p2g (vars, {"M"}, {"rho"}, true);
 
+#pragma omp single
+      {
+	timer.toc("p2g");
 #ifdef SAVE_OUTPUT
-    if (it % 50 == 0) 
-    {
-      // write particle data to file
-      const std::string ofilename = "particle";
-      const std::string ofileext = ".csv";
-      const std::string numfile = std::string(".") + std::to_string(it);
+	if (it % 50 == 0) 
+	  {
+	    // write particle data to file
+	    const std::string ofilename = "particle";
+	    const std::string ofileext = ".csv";
+	    const std::string numfile = std::string(".") + std::to_string(it);
       
-      std::ofstream outbuf (ofilename + numfile + ofileext);  
-      p.print<particles_t::output_format::csv> (outbuf);
+	    std::ofstream outbuf (ofilename + numfile + ofileext);  
+	    p.print<particles_t::output_format::csv> (outbuf);
 
-      outbuf.close ();
+	    outbuf.close ();
 
-      // write grid data to file
-      const std::string gfilename = std::string("grid.") + std::to_string(it) + std::string(".vts");
-      qg.vtk_export (gfilename.c_str(), vars);
+	    // write grid data to file
+	    const std::string gfilename = std::string("grid.") + std::to_string(it) + std::string(".vts");
+	    qg.vtk_export (gfilename.c_str(), vars);
       
-    }
+	  }
 #endif
-    
+      }
+    }
   }
-
+  
+  timer.print_report();
   return 0;
 }
--- Labs/2025/13-parallel/02-how_to_parallelize/MPM/write_velocity_field_json.m
+++ Labs/2025/13-parallel/02-how_to_parallelize/MPM/write_velocity_field_json.m
@@ -12,13 +12,16 @@ y = 0 : hy : ny*hy;
 vx = 1e-3*(2*pi*(1-y))(:);
 vy = 1e-3*(2*pi*(x-1))(:);
 
-in.num_particles = 100_000;
-in.x = randn(in.num_particles, 1) * .2/6 + 1.5;
-in.y = randn(in.num_particles, 1) * .2/6 + 1.5;
-in.iprops.label = [0:numel(in.x)](:);
+in.num_particles = 200e5;
+in.x             = randn(in.num_particles, 1) * .2/6 + 1.5;
+in.y             = randn(in.num_particles, 1) * .2/6 + 1.5;
+in.iprops.label  = [0:numel(in.x)](:);
 
-in.dprops = struct ('M',  2*pi*(.4/6)^2*ones(in.num_particles, 1)/in.num_particles, 'VX', zeros(in.num_particles, 1), 'VY', zeros(in.num_particles, 1));
-in.grid_vars = struct ('vx', vx, 'vy', vy, 'rho', zeros(num_global_nodes, 1));
+in.dprops = struct ('M',  2*pi*(.2/6)^2*ones(in.num_particles, 1)/in.num_particles,
+		    'VX', zeros(in.num_particles, 1), 'VY', zeros(in.num_particles, 1));
+
+in.grid_vars = struct ('vx', vx, 'vy', vy,
+		       'rho', zeros(num_global_nodes, 1));
 
 str = jsonencode (in);
 fid = fopen ('velocity.json', 'w');
