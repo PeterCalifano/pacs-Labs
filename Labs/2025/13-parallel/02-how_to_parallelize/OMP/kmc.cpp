5612f630ee735ab117073c7e6f59aea4ede39ce0
Carlo de Falco <carlo.defalco@polimi.it>
2025-09-09 00:26:00 +0200

    fixes

--- Labs/2025/13-parallel/02-how_to_parallelize/OMP/kmc.cpp
+++ Labs/2025/13-parallel/02-how_to_parallelize/OMP/kmc.cpp
@@ -4,7 +4,7 @@
 #include <fstream>
 #include <functional>
 #include <iostream>
-#include <random> 
+#include <random>
 #include <vector>
 #include <timer.h>
 
@@ -19,14 +19,14 @@ constexpr REAL   KT  = .1;                       // [J] thermal energy
 constexpr REAL   r   = 1e-2;                     // [m] particle radius
 constexpr REAL   mu  = 1.;                       // [kg s^-1 m_1] viscosity
 constexpr REAL   mob = 1. / 6. / M_PI / mu / r;  // [s kg^-1] mobility
-constexpr INT    Np  = 1000000;                  // [-] number of particles
-constexpr INT    SVST= 100;                      // [-] number of particles
-constexpr REAL   dt  = 1e-4;                     // [s] time step
-constexpr REAL   T   = 1.0;                      // [s] simulation time
-constexpr REAL   g   = 0.0;                      // [m s^-2] gravity
-constexpr REAL   m   = 2.0;                      // [kg] particle mass
+constexpr INT    Np  = 100000;                  // [-] number of particles
+constexpr INT    SVST= 5000;                     // [-] number of particles
+constexpr REAL   dt  = .5e-5;                    // [s] time step
+constexpr REAL   T   = 3.0;                      // [s] simulation time
+constexpr REAL   g   = 0.5;                       // [m s^-2] gravity
+constexpr REAL   m   = 1;                      // [kg] particle mass
 
-void fluid_velocity (REAL x, REAL y, REAL& vx, REAL& vy){ 
+void fluid_velocity (REAL x, REAL y, REAL& vx, REAL& vy){
   vx = 2. * M_PI * (1.-y);
   vy = 2. * M_PI * (x-1.);
 }
@@ -47,20 +47,23 @@ stepper {
 private :
   VECTOR &x;
   VECTOR &y;
-  
+
 public :
-  
+
   stepper (VECTOR &x_, VECTOR &y_)
     : x(x_), y(y_) { };
-  
-  void operator() (int n) {
+
+  void operator() (INT n) {
     REAL vx, vy;
     REAL fx, fy;
-    
-    // Compute fluid velocity and forces  
+
+    // Compute fluid velocity and forces
     fluid_velocity (x[n], y[n], vx, vy);
     particle_force (x[n], y[n], fx, fy);
-   
+
+    fluid_velocity (x[n] + (vx + mob*fx) * dt/2, y[n] + (vy + mob*fy) * dt/2, vx, vy);
+    particle_force (x[n] + (vx + mob*fx) * dt/2, y[n] + (vy + mob*fy) * dt/2, fx, fy);
+
     //update particles positions
     x[n] += (vx + mob*fx) * dt;
     y[n] += (vy + mob*fy) * dt;
@@ -69,25 +72,25 @@ public :
     walls (x[n], y[n]);
   };
 
-  
+
   void save(const std::string &filename) const {
     std::ofstream outFile (filename);
-    
+
     // Check if the file is open
     if (!outFile.is_open()) {
       std::cerr << "Error opening output file." << std::endl;
     }
-  
+
     // Write the header for the CSV file
     outFile << "%% Ball_X, Ball_Y \n";
 
-       
+
     // Write particle position and time to the output file
     for (INT n = 0; n < Np; ++n){
-      outFile << x[n] << "," << y[n] << "\n"; 
+      outFile << x[n] << "," << y[n] << "\n";
     }
-      
-    //Close the time-step-specific file 
+
+    //Close the time-step-specific file
     outFile.close();
   }
 };
@@ -100,24 +103,24 @@ int main() {
   // OR :
   // REAL *x = new REAL[Np];
   // REAL *y = new REAL[Np];
-  
+
   std::random_device rd;  // Will be used to obtain a seed for the random number engine
   std::mt19937 gen(rd()); // Standard mersenne_twister_engine seeded with rd()
-  std::uniform_real_distribution<> uniform(0.0, 1.);
-    
+  std::normal_distribution<> uniform(3*(xmax-xmin)/4, std::pow((xmax-xmin)/8, 2));
+
   // initialize particle positions
-  for (INT n = 0; n < Np; ++n) {  
-    x[n]=uniform(gen) * (xmax-xmin) + xmin;
-    y[n]=uniform(gen) * (ymax-ymin) + ymin;
+  for (INT n = 0; n < Np; ++n) {
+    x[n]=uniform(gen);
+    y[n]=uniform(gen);
   }
-    
+
   stepper state (x, y);
   cdf::timer::timer_t timer;
-  
+
   //Calculate the number of time steps
   INT Nt = std::ceil(T/dt);
   std::cout << "T = " << T << "; dt = " << dt << "; Nt = " << Nt << std::endl;
-    
+
   REAL  t = 0;  //initial time
   INT iff = 0;  //File index
 
@@ -127,15 +130,15 @@ int main() {
 
 #pragma omp parallel default(shared)
   {
-    for (INT it = 0; it<Nt; ++it){    
-    
+    for (INT it = 0; it<Nt; ++it){
+
       // Loop over all particles
 #pragma omp barrier
 #pragma omp single
       {
 	timer.tic("move partcles");
       }
-#pragma omp for 
+#pragma omp for
       for (INT n=0; n<Np; ++n)
 	state(n);
 
@@ -148,21 +151,21 @@ int main() {
 
 	if (printframe) {
 	  timer.tic("save");
-	  //Create a new CSV file for each time step    
+	  //Create a new CSV file for each time step
 	  auto particle_move_n = std::to_string(iff++);
 	  std::string particle_move = "particle_position_00000.csv";
 	  particle_move.replace (23-particle_move_n.length (), particle_move_n.length (), particle_move_n);
 	  std::cout << "iteration " << it;
 	  std::cout << ", saving file " << particle_move << std::endl;
-	  state.save (particle_move);      
+	  state.save (particle_move);
 	  timer.toc("save");
 	}
       }
-      
+
       t += dt;
     }
   }
-    
+
   std::cout << "Simulation complete. Data saved to particle_positions.csv.\n";
   // if using arrays we should free them manually
   // delete[] x;
